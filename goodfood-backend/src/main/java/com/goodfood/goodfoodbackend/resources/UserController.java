c6822aeef04faad67810f25438fde397e2873568
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-04-26 00:30:48 -0300

    feat: adição de annotations @RequestMapping e @Tag nas Controllers para corrigir endpoints

--- goodfood-backend/src/main/java/com/goodfood/goodfoodbackend/resources/UserController.java
+++ goodfood-backend/src/main/java/com/goodfood/goodfoodbackend/resources/UserController.java
@@ -1,33 +1,28 @@
 package com.goodfood.goodfoodbackend.resources;
 
+import com.goodfood.goodfoodbackend.models.User;
+import com.goodfood.goodfoodbackend.services.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.goodfood.goodfoodbackend.models.User;
-import com.goodfood.goodfoodbackend.services.UserService;
-
 @RestController
+@RequestMapping("/users")
+@Tag(name = "user", description = "the User API")
 @CrossOrigin
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("users")
+    @PostMapping
     public ResponseEntity<User> save(@RequestBody User user) {
-        
+
         user = userService.save(user);
 
         URI location = ServletUriComponentsBuilder
@@ -39,24 +34,24 @@ public class UserController {
         return ResponseEntity.created(location).body(user);
     }
 
-    @GetMapping("users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable int id) {
         User user = userService.getById(id);
         return ResponseEntity.ok(user);
     }
-    
-    @GetMapping("users")
+
+    @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userService.getAll());
     }
 
-    @PutMapping("users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Void> updateUser(@PathVariable int id, @RequestBody User userUpdate) {
         userService.update(id, userUpdate);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("users/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeUser(@PathVariable int id) {
         userService.deleteById(id);
         return ResponseEntity.noContent().build();
