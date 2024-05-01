2add3c3e02615e7e7a13665e4306786f74079b27
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-05-01 20:37:14 -0300

    feat(User.java): adiciona anotações Lombok e remove boilerplate

--- goodfood-backend/src/main/java/com/goodfood/goodfoodbackend/models/User.java
+++ goodfood-backend/src/main/java/com/goodfood/goodfoodbackend/models/User.java
@@ -7,91 +7,32 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
-@Table(name="TBL_USER")
+@Table(name = "user")
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String password;
 
-    @Column(nullable = true, length = 1024)
+    @Column(length = 1024)
     private String description;
 
-    @Column(nullable = true, length = 1024)
     private String telephone;
-
-    @ManyToOne
-    private Category category;
-
-    public User() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
 }
