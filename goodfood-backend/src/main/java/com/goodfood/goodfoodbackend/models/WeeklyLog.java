c86f268d62855a12cff8c6de07085f602ee01aac
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-05-19 12:13:44 -0300

    feat: cria nova entidade WeeklyLog

---
+++ goodfood-backend/src/main/java/com/goodfood/goodfoodbackend/models/WeeklyLog.java
@@ -0,0 +1,30 @@
+package com.goodfood.goodfoodbackend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "weekly_log")
+public class WeeklyLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double rating;
+    private Double weight;
+
+    private LocalDate endDate;
+
+    @Column(length = 1024)
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "diet_id")
+    private Diet diet;
+}
