57541b081f14041f29b0b8f7dc60e0ca9fddc91e
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-06-26 23:01:19 -0300

    feat(login): atualiza componente de login

--- goodfood-frontend/src/app/components/login/login.component.ts
+++ goodfood-frontend/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Role } from '../../interfaces/enums/Role';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +9,22 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username: string = '';
+  email: string = '';
   password: string = '';
   errorMessage: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) { }
 
   login() {
-    this.authService.login(this.username, this.password).subscribe(response => {
-      const role = this.authService.getUserRole();
-      if (role === 'CLIENT') {
-        this.router.navigateByUrl('home-client');
-      } else if (role === 'NUTRITIONIST') {
-        this.router.navigateByUrl('home-nutritionist');
-      }
-    }, error => {
-      this.errorMessage = 'Usuário ou senha incorretos. Por favor, tente novamente.';
-    });
+    this.authService.login(this.email, this.password)
+      .subscribe(response => {
+        this.errorMessage = '';
+
+        this.router.navigateByUrl(
+          this.authService.getUserRole() === Role.CLIENT ?
+            'home-client' : 'home-nutritionist');
+      }, error => {
+        this.errorMessage = 'Usuário ou senha incorretos. Por favor, tente novamente.';
+      });
   }
 }
