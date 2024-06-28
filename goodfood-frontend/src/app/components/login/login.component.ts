6e61cebeae461e3ca9fec3ac31a76f9ee63c3c06
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2024-06-28 20:53:30 -0300

    feat(login): extract method e adiciona validação no ngOnInit

--- goodfood-frontend/src/app/components/login/login.component.ts
+++ goodfood-frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { Role } from '../../interfaces/enums/Role';
@@ -8,23 +8,33 @@ import { Role } from '../../interfaces/enums/Role';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
   email: string = '';
   password: string = '';
   errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) { }
 
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated())
+      this.navigate();
+  }
+
   login() {
     this.authService.login(this.email, this.password)
       .subscribe(response => {
         this.errorMessage = '';
-
-        this.router.navigateByUrl(
-          this.authService.getUserRole() === Role.CLIENT ?
-            'home-client' : 'home-nutritionist');
+        this.navigate();
       }, error => {
         this.errorMessage = 'Usuário ou senha incorretos. Por favor, tente novamente.';
+        console.log(error);
       });
+  }
+
+  navigate() {
+    this.router.navigateByUrl(
+      this.authService.getUserRole() === Role.CLIENT ?
+        'home-client' : 'home-nutritionist');
   }
 }
